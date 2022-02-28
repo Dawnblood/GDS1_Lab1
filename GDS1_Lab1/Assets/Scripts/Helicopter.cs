ebca646fa687fed208ee3d74314d571887f5c09e
Dawnblood <69774919+Dawnblood@users.noreply.github.com>
2022-02-28 12:36:50 +1100

    Randomising Placement of Soldiers

--- GDS1_Lab1/Assets/Scripts/Helicopter.cs
+++ GDS1_Lab1/Assets/Scripts/Helicopter.cs
@@ -7,16 +7,19 @@ using UnityEngine.UI;
 public class Helicopter : MonoBehaviour
 {
 
-    public Rigidbody2D helicopter;
+    public Rigidbody2D helicopterRGB;
     public float moveSpeed = 5f;
     Vector2 movement;
 
-    GameObject LevelManager;
+    GameObject levelManager;
+    GameObject helicopter;
 
     // Start is called before the first frame update
     void Start()
     {
-        LevelManager = GameObject.FindGameObjectWithTag("LevelManager");
+        levelManager = GameObject.FindGameObjectWithTag("LevelManager");
+
+        helicopter = GameObject.Find("Helicopter");
     }
 
     // Update is called once per frame
@@ -24,11 +27,20 @@ public class Helicopter : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && helicopter.transform.rotation.y == 0)
+        {
+            helicopter.transform.Rotate(new Vector3(0, -180, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && helicopter.transform.rotation.y > 0)
+        {
+            helicopter.transform.Rotate(new Vector3(0, 180, 0));
+        }
     }
 
     private void FixedUpdate()
     {
-        helicopter.MovePosition(helicopter.position + movement * moveSpeed * Time.fixedDeltaTime);
+        helicopterRGB.MovePosition(helicopterRGB.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -37,9 +49,9 @@ public class Helicopter : MonoBehaviour
         {
             SceneManager.LoadScene("Game Over", LoadSceneMode.Single);
         }
-        else if (collision.tag == "Hospital" && LevelManager.GetComponent<LevelManager>().soldierHelicopter > 0)
+        else if (collision.tag == "Hospital" && levelManager.GetComponent<LevelManager>().soldierHelicopter > 0)
         {
-            LevelManager.GetComponent<LevelManager>().soldierHelicopter = 0;
+            levelManager.GetComponent<LevelManager>().soldierHelicopter = 0;
         }
     }
 }
--- GDS1_Lab1/Assets/Scripts/LevelManager.cs
+++ GDS1_Lab1/Assets/Scripts/LevelManager.cs
@@ -6,7 +6,11 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject[] Soldiers;
+    public GameObject soldier;
+    public int numSoldiers;
+
+    public Collider2D[] colliders;
+    public float radius;
 
     public int soldiersSaved;
     public Text soldiersSavedText;
@@ -17,7 +21,13 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Soldiers = GameObject.FindGameObjectsWithTag("Soldier");
+
+        numSoldiers = Random.Range(4, 8);
+
+        for (int i = 0; i < numSoldiers; i++)
+        {
+            SpawnSoldiers();
+        }
 
         soldiersSaved = 0;
 
@@ -36,10 +46,59 @@ public class LevelManager : MonoBehaviour
             SceneManager.LoadScene("Level");
         }
 
-        if (soldiersSaved == Soldiers.Length)
+        if (soldiersSaved == numSoldiers)
         {
             SceneManager.LoadScene("You Win", LoadSceneMode.Single);
         }
 
+    }
+
+    public void SpawnSoldiers()
+    {
+        Vector3 spawnPos = new Vector3(0, 0 , 0);
+        bool canSpawn = false;
+
+        while (!canSpawn)
+        {
+            float spawnPosX = Random.Range(5.4f, 11.45f);
+            float spawnPosY = Random.Range(4f, -4f);
+            spawnPos = new Vector3(spawnPosX, spawnPosY, 0);
+
+            canSpawn = PreventSpawnOverlap(spawnPos);
+
+            if (canSpawn)
+            {
+                break;
+            }
+        }
+
+        GameObject newSoldier = Instantiate(soldier, spawnPos, Quaternion.identity) as GameObject;
+    }
+
+    private bool PreventSpawnOverlap(Vector3 spawnPos)
+    {
+        colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Vector3 centerPoint = colliders[i].bounds.center;
+            float width = colliders[i].bounds.extents.x;
+            float height = colliders[i].bounds.extents.y;
+
+            float leftExtent = centerPoint.x - width;
+            float rightExtent = centerPoint.x + width;
+            float lowerExtent = centerPoint.y - height;
+            float upperExtent = centerPoint.y + height;
+
+            if (spawnPos.x >= leftExtent && spawnPos.x <= rightExtent)
+            {
+                if (spawnPos.y >= lowerExtent && spawnPos.y <= upperExtent)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
